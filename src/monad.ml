056a56f7d78fba42981ad80931d869b9b913a2b9
redianthus <redopam@pm.me>
2026-05-27 14:04:33 +0200

    more less

--- src/monad.ml
+++ src/monad.ml
@@ -1,6 +1,6 @@
 (** A monad representing computation that can be cooperatively scheduled.
-    Computations can yield ([Yield]), and split into two non deterministic choices ([Choice]).
-    They can also fail/stop ([Ok] versus [Error]). *)
+    Computations can yield ([Yield]), and split into two non deterministic
+    choices ([Choice]). They can also fail/stop ([Ok] versus [Error]). *)
 
 type ('a, 'err, 'prio, 'state) schedulable =
   | Ok of 'a * 'state
@@ -16,70 +16,48 @@ type ('a, 'err, 'prio, 'state) t =
 
 (* Schedulable+State monadic boilerplate *)
 
-let[@inline] rec bind_schedulable (x : ('a, 'err, 'prio, 'state) schedulable)
-  (f : 'a -> 'state -> ('b, 'err, 'prio, 'state) schedulable) :
-  ('b, 'err, 'prio, 'state) schedulable =
-  match x with
+let[@inline] rec bind_schedulable f = function
   | Ok (v, state) -> f v state
   | Error e -> Error e
-  | Yield (prio, step) -> Yield (prio, fun () -> bind_schedulable (step ()) f)
-  | Choice (a, b) -> Choice (bind_schedulable a f, bind_schedulable b f)
+  | Yield (prio, step) -> Yield (prio, fun () -> bind_schedulable f (step ()))
+  | Choice (a, b) -> Choice (bind_schedulable f a, bind_schedulable f b)
 
-let[@inline] bind (x : ('a, 'err, 'prio, 'state) t)
-  (f : 'a -> ('b, 'err, 'prio, 'state) t) : ('b, 'err, 'prio, 'state) t =
- fun (state : 'state) -> bind_schedulable (x state) f
+let[@inline] bind f x = fun state -> bind_schedulable f (x state)
 
-let[@inline] rec map_schedulable (f : 'a -> 'b)
-  (x : ('a, 'err, 'prio, 'state) schedulable) :
-  ('b, 'err, 'prio, 'state) schedulable =
-  match x with
+let[@inline] rec map_schedulable f = function
   | Ok (v, state) -> Ok (f v, state)
   | Error e -> Error e
   | Yield (prio, step) -> Yield (prio, fun () -> map_schedulable f (step ()))
   | Choice (a, b) -> Choice (map_schedulable f a, map_schedulable f b)
 
-let[@inline] map (f : 'a -> 'b) (x : ('a, 'err, 'prio, 'state) t) :
-  ('b, 'err, 'prio, 'state) t =
- fun (state : 'state) -> map_schedulable f (x state)
+let[@inline] map f x = fun state -> map_schedulable f (x state)
 
-let[@inline] return (x : 'a) : ('a, _, _, 'state) t =
- fun (state : 'state) -> Ok (x, state)
+let[@inline] return x = fun state -> Ok (x, state)
 
-let[@inline] ( let* ) (x : ('a, 'err, 'prio, 'state) t) f : _ t = bind x f
+let[@inline] ( let* ) x f = bind f x
 
-let[@inline] ( let+ ) (x : ('a, 'err, 'prio, 'state) t) (f : 'a -> 'b) :
-  ('b, 'err, 'prio, 'state) t =
-  map f x
+let[@inline] ( let+ ) x f = map f x
 
 (** State operations. *)
 
-let[@inline] fold_state (f : 'state -> 'a) : ('a, 'err, 'prio, 'state) t =
- fun (state : 'state) -> Ok (f state, state)
+let[@inline] fold_state f = fun state -> Ok (f state, state)
 
-let[@inline] map_state (f : 'state -> 'state) : (unit, 'err, 'prio, 'state) t =
- fun (state : 'state) -> Ok ((), f state)
+let[@inline] map_state f = fun state -> Ok ((), f state)
 
 (** Symbolic execution primitives. *)
 
 (* Create two new branches, they do not yield so the yield should be created manually! *)
-let[@inline] choose (x : ('a, 'err, 'prio, 'state) t)
-  (y : ('a, 'err, 'prio, 'state) t) : ('a, 'err, 'prio, 'state) t =
- fun state -> Choice (x state, y state)
-
+let[@inline] choose x y = fun state -> Choice (x state, y state)
 (* Yield the current branch (i.e. add it to the work queue so that it gets executed later. )*)
-let[@inline] yield (prio : 'prio) : (unit, 'err, 'prio, 'state) t =
- fun (state : 'state) -> Yield (prio, fun () -> Ok ((), state))
+
+let[@inline] yield prio = fun state -> Yield (prio, fun () -> Ok ((), state))
 
 (* Child will be a new branch that immediately yields, and parent will execute directly without yielding. *)
-let[@inline] fork ~(parent : ('a, 'err, 'prio, 'state) t)
-  ~(child : 'prio * ('a, 'err, 'prio, 'state) t) : ('a, 'err, 'prio, 'state) t =
+let[@inline] fork ~parent ~child =
   let prio, child = child in
-  let child = bind (yield prio) (fun () -> child) in
+  let child = bind (fun () -> child) (yield prio) in
   choose parent child
 
-let[@inline] fail (err : 'err) : ('a, 'err, 'prio, 'state) t =
- fun (_state : 'state) -> Error err
+let[@inline] fail err = fun _state -> Error err
 
-let[@inline] run (f : ('a, 'err, 'prio, 'state) t) (state : 'state) :
-  ('a, 'err, 'prio, 'state) schedulable =
-  f state
+let[@inline] run f state = f state
